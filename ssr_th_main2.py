ccb44cec3d6adbba05abb667857c1d7512b59b68
chibaf <chibaf@mac.com>
2024-11-03 00:07:23 +0900

    Add files via upload

--- ssr_th_main2.py
+++ ssr_th_main2.py
@@ -14,30 +14,25 @@ from time import sleep
 #pi_pwm.start(0)				#start PWM of required Duty Cycle 
 
 i=1
-q =queue.Queue()  # queue which stores a result of a thread
-th1 = threading.Thread(target=ssr,args=(8,3,3,q),daemon=True)
-th2 = threading.Thread(target=ssr,args=(18,2,2,q),daemon=True)
+q1 =queue.Queue()  # queue which stores a result of a thread
+q2 =queue.Queue()  # queue which stores a result of a thread
+th1 = threading.Thread(target=ssr,args=(8,3,3,q1),name="t1",daemon=True)
+th2 = threading.Thread(target=ssr,args=(18,2,2,q2),name="t2",daemon=True)
 th1.start()
 th2.start()
-print("start thread: "+str(i))
+#print("start thread: "+str(i))
 #th.join()
 while True:
   try:
-    if threading.active_count()==0:
+    if threading.active_count()==1:
 #      data = q.get()
 #      print("thread returns "+data)
       i=i+1
-      if int(data)>=0:
-        pi_pwm.ChangeDutyCycle(100)
-        print("SSR ON")
-      else:
-        pi_pwm.ChangeDutyCycle(0)
-        print("SSR OFF")
-        th1 = threading.Thread(target=ssr,args=(8,3,3,q),daemon=True)
-        th2 = threading.Thread(target=ssr,args=(18,2,2,q),daemon=True)
-        th1.start()
-        th2.start()
-      print("start thread: "+str(i))
+      th1 = threading.Thread(target=ssr,args=(8,3,3,q1),name="t1",daemon=True)
+      th2 = threading.Thread(target=ssr,args=(18,2,2,q2),name="t2",daemon=True)
+      th1.start()
+      th2.start()
+#      print("start thread: "+str(i))
   except KeyboardInterrupt:
     print("Keyboard Interrupt")
     GPIO.cleanup()

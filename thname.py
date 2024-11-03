2c03fd38e910640eacde2ac6f7e22d429c50714a
chibaf <chibaf@mac.com>
2024-11-03 09:01:26 +0900

    Add files via upload

---
+++ thname.py
@@ -0,0 +1,9 @@
+import threading
+
+def test():
+    print("test")
+    print(threading.current_thread().name) # Here
+                                                              
+t = threading.Thread(target=test, name="test_thread")
+t.start()                         # Thread name
+t.join()

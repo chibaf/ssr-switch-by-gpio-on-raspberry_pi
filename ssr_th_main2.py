797423643d7d1be513b6bda7b9a42bb7bd8f85f2
chibaf <chibaf@mac.com>
2024-11-03 11:08:43 +0900

    Add files via upload

--- ssr_th_main2.py
+++ ssr_th_main2.py
@@ -5,8 +5,14 @@ from ssr_sw_th import ssr
 import RPi.GPIO as GPIO
 import datetime
 from time import sleep
+import os
 
-i=1
+j1=1
+pin_id1=str(8)
+path1='./go'+pin_id1+'.txt'
+j2=1
+pin_id2=str(18)
+path2='./go'+pin_id2+'.txt'
 q1 =queue.Queue()  # queue which stores a result of a thread
 q2 =queue.Queue()  # queue which stores a result of a thread
 th1 = threading.Thread(target=ssr,args=(8,3,3,q1),name="th1",daemon=True)
@@ -17,18 +23,25 @@ th2.start()
 th2.join()
 print(th1.name)
 print(th2.name)
+print("1: thread started")
 
 while True:
   try:
-    if threading.active_count()==1:
-#      data = q.get()
-#      print("thread returns "+data)
-      i=i+1
-      th1 = threading.Thread(target=ssr,args=(8,3,3,q1),name="t1",daemon=True)
-      th2 = threading.Thread(target=ssr,args=(18,2,2,q2),name="t2",daemon=True)
-      th1.start()
-      th2.start()
-#      print("start thread: "+str(i))
+    if threading.active_count()==3:
+      continue
+    else:
+     is_file1=os.path.isfile(path1)
+     if is_file1:
+       j1=j1+1
+       print("th1:",str(j1))
+       th1 = threading.Thread(target=ssr,args=(8,3,3,q1),name="th1",daemon=True)
+       th1.start()
+     is_file2=os.path.isfile(path2)
+     if is_file2:
+       j2=j2+1
+       print("th2:",str(j2))
+       th2 = threading.Thread(target=ssr,args=(18,2,2,q2),name="th2",daemon=True)
+       th2.start()
   except KeyboardInterrupt:
     print("Keyboard Interrupt")
     GPIO.cleanup()
